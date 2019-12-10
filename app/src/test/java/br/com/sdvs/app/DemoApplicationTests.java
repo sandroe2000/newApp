4791dc8a539910df9519081abee6f63e200a60d9
Sandro Cesario Di Espindula <sandroe2000>
2019-12-10 00:18:32 -0300

    Integrando CRUD

--- app/src/test/java/br/com/sdvs/app/DemoApplicationTests.java
+++ app/src/test/java/br/com/sdvs/app/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.sdvs.app.app;
+package br.com.sdvs.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
